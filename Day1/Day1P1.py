9a113349cd5c44078314e69aa30e4ccb14932535
Braden <bradenrosengren@gmail.com>
2021-12-01 00:34:35 -0600

    Day1 Completed

---
+++ Day1/Day1P1.py
@@ -0,0 +1,14 @@
+def read_integers(filename):
+	with open(filename) as f:
+		return [int(x) for x in f]
+
+
+depths = read_integers("Day1IN.txt")
+
+count = 0
+
+for i in range(1, len(depths)):
+	if depths[i] > depths[i-1]:
+		count += 1
+
+print(count)
---
+++ Day1/Day1P2.py
@@ -0,0 +1,16 @@
+def read_integers(filename):
+	with open(filename) as f:
+		return [int(x) for x in f]
+
+
+depths = read_integers("Day1IN.txt")
+
+count = 0
+
+for i in range(3, len(depths)):
+	cur = depths[i]+depths[i-1]+depths[i-2]
+	prev = depths[i-1]+depths[i-2]+depths[i-3]
+	if(cur > prev):
+		count += 1
+
+print(count)
