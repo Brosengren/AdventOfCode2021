fab75989ad66642c5b91fb5b0e32bede168eff03
Braden Rosengren <bradenrosengren@gmail.com>
2021-12-02 11:05:30 -0600

    Day02

--- Day02/Day02P1.py
+++ Day02/Day02P1.py
@@ -1,1 +1,19 @@
+def read_file(filename):
+	with open(filename) as f:
+		return f.readlines()
 
+
+input = read_file("Day02IN.txt")
+movement = [x.strip('\n').split(' ') for x in input]
+#print(movement)
+h = 0
+d = 0
+
+for travel in movement:
+	if   (travel[0].lower() == "forward"):
+		h += int(travel[1])
+	elif (travel[0].lower() == "down"):
+		d += int(travel[1])
+	elif (travel[0].lower() == "up"):
+		d -= int(travel[1])
+print(h*d)
--- Day02/Day02P2.py
+++ Day02/Day02P2.py
@@ -1,1 +1,21 @@
+def read_file(filename):
+	with open(filename) as f:
+		return f.readlines()
 
+
+input = read_file("Day02IN.txt")
+movement = [x.strip('\n').split(' ') for x in input]
+
+h = 0
+d = 0
+a = 0
+
+for travel in movement:
+	if   (travel[0].lower() == "forward"):
+		h += int(travel[1])
+		d += int(travel[1]) * a
+	elif (travel[0].lower() == "down"):
+		a += int(travel[1])
+	elif (travel[0].lower() == "up"):
+		a -= int(travel[1])
+print(h*d)
