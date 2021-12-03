368b45cf55ea610d64b0494712e182f0cb0e1492
Braden <bradenrosengren@gmail.com>
2021-12-03 01:21:18 -0600

    Day03

--- Day03/Day03P1.py
+++ Day03/Day03P1.py
@@ -1,1 +1,34 @@
+def read_file(filename):
+	with open(filename) as f:
+		return f.readlines()
 
+
+input = read_file("Day03IN.txt")
+report = [x.strip('\n') for x in input]
+countlist = [0 for x in range(len(report[0]))]
+gamma = list(countlist)
+epslion = list(countlist)
+for row in report:
+    for col in range(len(row)):
+        if row[col] == '1':
+            countlist[col] += 1
+        else:
+            countlist[col] -= 1
+print(countlist)
+
+for x in range(len(countlist)):
+    if countlist[x] > 0:
+        gamma[x] = 1
+        epslion[x] = 0
+    else:
+        gamma[x] = 0
+        epslion[x] = 1
+
+print(gamma)
+print(epslion)
+a = int("".join(str(i) for i in gamma),2)
+print(a)
+b = int("".join(str(i) for i in epslion),2)
+print(b)
+
+print(a*b)
--- Day03/Day03P2.py
+++ Day03/Day03P2.py
@@ -1,1 +1,70 @@
+def read_file(filename):
+	with open(filename) as f:
+		return f.readlines()
 
+
+input = read_file("Day03IN.txt")
+report = [x.strip('\n') for x in input]
+numbits =len(report[0])
+ogrlist = list(report)
+csrlist = list(report)
+#print(ogrlist)
+for col in range(numbits):
+    if len(ogrlist) == 1:
+        continue
+    count = 0
+    for row in ogrlist:
+        if row[col] == '1':
+            count += 1
+        else:
+            count -= 1
+#    print(count)
+    length = len(ogrlist)
+    row = 0
+    while (row < length):
+        if count >= 0:
+            if ogrlist[row][col] == '0':
+                ogrlist.remove(ogrlist[row])
+                row -= 1
+                length -= 1
+        else:
+            if ogrlist[row][col] == '1':
+                ogrlist.remove(ogrlist[row])
+                row -= 1
+                length -= 1
+        row += 1
+#    print(ogrlist)
+
+for col in range(numbits):
+    if len(csrlist) == 1:
+        continue
+    count = 0
+    for row in csrlist:
+        if row[col] == '1':
+            count += 1
+        else:
+            count -= 1
+#    print(count)
+    length = len(csrlist)
+    row = 0
+    while (row < length):
+        if count >= 0:
+            if csrlist[row][col] == '1':
+                csrlist.remove(csrlist[row])
+                row -= 1
+                length -= 1
+        else:
+            if csrlist[row][col] == '0':
+                csrlist.remove(csrlist[row])
+                row -= 1
+                length -= 1
+        row += 1
+#    print(csrlist)
+
+
+ogr = int(ogrlist[0],2)
+csr = int(csrlist[0],2)
+
+print(ogr)
+print(csr)
+print(ogr * csr)
