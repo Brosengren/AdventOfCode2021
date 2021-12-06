bf037d79d5887815ada27980115edeb4fc9eafa4
Braden <bradenrosengren@gmail.com>
2021-12-06 13:14:58 -0600

    Day04 Partial

--- Day04/Day04P1.py
+++ Day04/Day04P1.py
@@ -1,1 +1,21 @@
+input = []
+with open("Day04INtest.txt") as f:
+    for line in f:
+        input.append(line.strip())
+
+#print(input)
+picks = input[0]
+
+boards = []
+board = []
+for i in range(2, len(input), 6):
+    for x in range(5):
+        board.append([int(y) for y in input[i+x].split(' ') if y != ''])
+    else:
+        if board != []:
+            boards.append(board)
+            board = []
+print(picks)
+print(boards)
+
 
